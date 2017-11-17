4128de15872e20155a13de44f022ff424444ce49
holgerhansson <holger.hansson@ria.ee>
2017-11-17 22:27:09 +0200

    Redone

--- README.md
+++
@@ -1,1 +0,0 @@
-# holgerhansson.github.io
--- js/ftt.js
+++ js/ftt.js
@@ -1,7 +1,31 @@
+currentCycle = 1;
+$(document).ready(function() {
+	$('#datetimepicker1').datetimepicker({
+		    	format: 'DD/MM/YYYY',
+		    	locale: "et",
+		    	minDate: 0
+	});
+});
+
 $(document).ready(function() {
     $('#form_to_table').click(function() {
-    console.log("Clicked!")
-      ftt($('datepicker').val());
+
+    	$( ".input" + currentCycle ).each( function(i, elem) {
+    		jQuery('[name="tellimusLeht[' + i + '][' + currentCycle + ']"]').val(elem.value);
+		});
+
+		for (i = 1; i <= currentCycle; i++) {
+		    $( ".input" + i ).hide();
+		} 
+		
+		currentCycle += 1;
+		$('#datetimepicker' + currentCycle).datetimepicker({
+	    	format: 'DD/MM/YYYY',
+	    	locale: "et",
+	    	minDate: 0
+		});
+		$( ".input" + currentCycle ).show();
+		$("#demo-2").tabularInput("addRow");		
     });
 });
 
